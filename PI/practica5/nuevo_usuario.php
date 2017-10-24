12c53010112bc164a4261fb06eb6242c5e8e4d10
Alberto <alberto.berenguer.97@gmail.com>
2017-10-24 17:58:25 +0200

    mis cosas

--- PI/practica5/nuevo_usuario.php
+++ PI/practica5/nuevo_usuario.php
@@ -17,47 +17,40 @@
 		<p id="letras_logo"><span lang="en">Pictures & Images</span></p></a>
 	</header>
 	<main>
-		<h1 id="titulo_registro">Formulario de registro</h1>
-		<p id="obligatorio">IMPORTANTE: Todos los campos de este formulario son obligatorios.</p>
-		<form method="POST" action="nuevo_usuario.php">
+		<!--Gestion de la peticion -->
+		<?php
+			$usuario = $_POST['usuario'];
+			$mail = $_POST['mail'];
+			$sexo = $_POST['sexo'];
+			$nacimiento = $_POST['nacimiento'];
+			$ciudad = $_POST['ciudad'];
+			$pais = $_POST['pais'];
+			$foto = $_POST['foto'];
+		?>
+
+		<h1 id="titulo_registro">Datos registrados</h1>
 			<p><label for="usuario">Nombre de usuario :</label>
-				<input type="text" name="usuario" id="usuario" required autofocus>
-			</p>
-			<p><label for="pass">Contraseña :</label>
-				<input type="password" name="pass" id="pass" required>
-			</p>
-			<p><label for="pass2">Repetir contraseña :</label>
-				<input type="password" name="pass2" id="pass2" required>
+				<input type="text" name="usuario" id="usuario"  value="<?php echo $usuario; ?>" readonly>
 			</p>
 			<p><label for="mail">Email :</label>
-				<input type="email" name="mail" id="mail" required>
+				<input type="email" name="mail" id="mail"  value="<?php echo $mail; ?>" readonly>
 			</p>
-				<fieldset>
-				<legend>Sexo :</legend>
-					<p><label><input type="radio" name="sexo" value="Hombre" checked> Hombre </label></p>
-					<p><label><input type="radio" name="sexo" value="Mujer"> Mujer </label></p>
-					<p><label><input type="radio" name="sexo" value="Otro"> Otro </label></p>
-				</fieldset>
+				<p><label for="text">Sexo :</label>
+				<input type="email" name="sexo" id="sexo" value="<?php echo $sexo; ?>" readonly>
+			</p>
 			<p>
 				<label for="nacimiento">Fecha de nacimiento :</label>
-				<input type="date" name="nacimiento" id="nacimiento" required>
+				<input type="date" name="nacimiento" id="nacimiento"  value="<?php echo $nacimiento; ?>" readonly>
 			</p>
 			<p>
 				<label for="ciudad">Ciudad :</label>
-				<input type="text" name="ciudad" id="ciudad" required>
+				<input type="text" name="ciudad" id="ciudad"  value="<?php echo $ciudad; ?>" readonly>
 			</p>
 			<p>
 				<label for="pais">Pais de residencia :</label>
-				<input type="text" name="pais" id="pais" required>
+				<input type="text" name="pais" id="pais" value="<?php echo $pais; ?>" readonly>
 			</p>
-			<p>
-				<label for="foto">Foto de perfil :</label>
-				<input type="file" name="foto" id="foto" required>
-			</p>
-			<p>
-				<input id="boton_registro" type="submit" value="Registrar">
-			</p>
-		</form>
+			<a id="volver" href="index.php">Volver al Inicio</a>
 	</main>
 	<p id="copy">Todos los derechos reservados© 2017</p>
 </body>
--- PI/practica5/solicitar_album.php
+++ PI/practica5/solicitar_album.php
@@ -13,43 +13,43 @@
 </head>
 <body>
 	<header>
-			<a href="index.html"><img id="logo" src="imagenes/logo_cabecera.png" alt="Logotipo PI" width="256" height="256">
+			<a href="index.php"><img id="logo" src="imagenes/logo_cabecera.png" alt="Logotipo PI" width="256" height="256">
 			<p id="letras_logo"><span lang="en">Pictures & Images</span></p></a>
 	</header>
 	<nav>
 		<ul>
-			<li><a href="formulario_busqueda.html">Buscar</a></li>
-			<li><a href="registro.html">Registrarse</a> </li>
+			<li><a href="formulario_busqueda.php">Buscar</a></li>
+			<li><a href="registro.php">Registrarse</a> </li>
 		</ul>		
 	</nav>
 	<main id="solbum">
 		<h1 id="titulo_solicitar_album">Solicitar álbum</h1>
 			<p style="text-align: center;">En esta sección de la web podrás rellenar los datos necesarios (Información de contacto, detalles del álbum y de envio) para recibir en tu casa tus álbumes de PI - Pictures & Images. Puedes consultar las diferentes tarifas al final de la página.</p>
-			<form method="GET" action="resultado_album.html">
+			<form method="POST" action="resultado_album.php">
 				<fieldset>
 					<legend><h3>Información de contacto</h3></legend>
 
 						<p><label for="nombreUsuario">Nombre (*)</label><input type="text" name="nombreUsuario" id="nombreUsuario" placeholder="Ej. Alejandro (max. 200 carácteres)" maxlength="200" size="40" autofocus required=""></p>
 						<p><label for="email">Email (*)</label><input type="email" name="email" id="email" placeholder="Ej. email@gmail.com (max. 200 carácteres)" maxlength="200" size="40" required=""></p>
-						<p><label for="telefono">Teléfono (*)</label><input type="tel" name="telefono" id="telefono" placeholder="Teléfono de contacto (8 dígitos)" maxlength="8" size="40"></p>
+						<p><label for="telefono">Teléfono (*)</label><input type="tel" name="telefono" id="telefono" placeholder="Teléfono de contacto (9 dígitos)" maxlength="9" size="40"></p>
 				</fieldset>
 				<fieldset>
 					<legend><h3>Detalles del álbum</h3></legend>
 						<p><label for="tituloAlbum">Título (*)</label><input type="text" name="tituloAlbum" id="tituloAlbum" placeholder="Ej. Vacaciones (max. 200 carácteres)" maxlength="200" size="40" required="">
 						</p>
 						<p><label for="albumUsuario">Álbum de PI (*)</label>
-							<select id="albumUsuario" required="">
+							<select id="albumUsuario" name="albumUsuario" required="">
 								<option value="">Elige una opción</option>
-								<option value="1">Album1</option>
-								<option value="2">Album2</option>
-								<option value="3">Album3</option>
+								<option value="Album1">Album1</option>
+								<option value="Album2">Album2</option>
+								<option value="Album3">Album3</option>
 							</select>
 						 </p>
 						<p><label for="textoAdicional">Texto adicional</label><textarea rows="4" cols="50" name="textoAdicional" id="textoAdicional" placeholder="dedicatoria, descripcion, etc. (max. 4000 carácteres)" maxlength="4000" ></textarea></p>
 						<p><label for="colorPortada">Color de portada</label><input type="color" name="colorPortada" id="colorPortada" ></p>
 						<p><label for="numeroCopias">Copias: </label><input type="number" name="numeroCopias" id="numeroCopias" min="1" max="100" value="1"></p>
 						<p><label for="resolucion">Resolución de impresión</label>
-							<select id="resolucion"> 
+							<select id="resolucion" name="resolucion"> 
 								<option value="150">150 dpi</option>
 							  	<option value="300">300 dpi</option>
 							 	<option value="450">450 dpi</option>
@@ -75,7 +75,7 @@
 						<p>
 							<label for="codigoPostal">Código postal: </label><input type="text" name="codigoPostal" id="codigoPostal" placeholder="Ej. 03698" maxlength="5" size="10" required="">
 							<label for="localidad">Localidad: </label>
-								<select id="localidad" required="">
+								<select id="localidad" name="localidad" required="">
 								 	<option value="">Elige una opción</option>
 									<option value="localidad1">Localidad1</option>
 								  	<option value="localidad2">Localidad2</option>
@@ -83,7 +83,7 @@
 								 	<option value="localidad4">Localidad4</option>
 								</select>
 								<label for="provincia">Provincia</label>
-								<select id="provincia" required="">
+								<select id="provincia" name="provincia" required="">
 									<option value="">Elige una opción</option>
 									<option value="provincia1">Provncia1</option>
 								  	<option value="provincia2">Provincia2</option>
@@ -91,7 +91,7 @@
 								 	<option value="provincia4">Provincia4</option>
 								</select>
 								<label for="pais">Pais</label>
-								<select id="pais" required=""> 
+								<select id="pais" name="pais" required=""> 
 									<option value="">Elige una opción</option>
 									<option value="pais1">País1</option>
 								  	<option value="pais2">País2</option>
@@ -99,7 +99,7 @@
 								 	<option value="pais4">País4</option>
 								</select>
 						</p>
-						<p><label for="fechaRececpcion">Fecha de recepción (dd/mm/aaaa)</label><input type="date" name="fechaRececpcion" id="fechaRececpcion" size="15"></p>
+						<p><label for="fechaRececpcion">Fecha de recepción (dd/mm/aaaa)</label><input type="date" name="fechaRecepcion" id="fechaRececpcion" size="15" required></p>
 					</fieldset>
 
 					<h3>Tarifas</h3>
