ecf87256f65233a7f5184d5c910d76bf55a6bc17
Alejandro Domenech <a.domenechvicedo@gmail.com>
2017-10-28 14:05:01 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/albertik97/DAW""
    This reverts commit 1dad7857714637beaf56f0dbee5bdb1f49cafa30.

--- PI/practica5/plantillas/nav_usuario_no_identificado.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="formulario_busqueda.php">Buscar</a></li>
-		<li><a href="registro.php">Registrarse</a> </li>
-		<li><a href="index.php">Iniciar sesion</a></li>
-	</ul>
-</nav>
