32e7b8e589007d3de8340f5bbbe1234020a0ccd4
Alberto <alberto.berenguer.97@gmail.com>
2017-11-22 17:00:47 +0100

    xd

--- PI/practica8/menu_usuario.php
+++ PI/practica8/menu_usuario.php
@@ -10,6 +10,17 @@ session_start();
 	if(isset($_SESSION['user']))
 	{
 		require_once('plantillas/nav_usuario_identificado.php');
+
+		$mysqli = @new mysqli('localhost','web_user','','pibd');
+		if($mysqli->connect_errno){
+			echo "Error al conectarse a la base de datos";
+		}	
+
+		$consulta= 'select * from usuarios where NomUsuario='. $_SESSION['user'];
+		if(!($res=$mysqli->query($consulta))){
+			echo "Error al realziar la consulta";
+		}
+		$usuario = $res->fetch_assoc();
 ?>
 	<main id="main_usuario">
 		<fieldset id="misdatos">
@@ -17,7 +28,7 @@ session_start();
 			<p><img id="imagen_perfil" src="imagenes/chocu.jpg" alt="foto perfil" width="100" height="100"></p>
 			<table>
 				<tr>
-					<td>Usuario</td> <td>el_chocu</td>
+					<td>Usuario</td> <td><?php?></td>
 				</tr>
 
 				<tr><td>Nombre :</td> <td>Chocu Martínez de la Selva</td>
@@ -30,10 +41,10 @@ session_start();
 				</tr>
 				<tr><td>Ciudad :</td> <td>Lopburi</td></tr>
 			</table>
-			<p><button id="edit_perfil" >Modificar datos</button></p>
+			<p><a href="mis_datos.php" id="edit_perfil" >Modificar datos</a></p>
 		</fieldset>
 		<hr>
-		<a class="botones_perfil" href="#" ><img src="imagenes/delete.png" alt="">Darse de baja</a>
+		<a class="botones_perfil" href="dar_baja.php" ><img src="imagenes/delete.png" alt="">Darse de baja</a>
 		<a class="botones_perfil" href="crear_album.php" ><img src="imagenes/add.png" alt="">Crear álbum nuevo</a>
 		<a class="botones_perfil" href="anyadir_foto.php" ><img src="imagenes/add.png" alt="">  Añadir foto a álbum</a>
 		<a class="botones_perfil" href="mis_albumes.php" ><img src="imagenes/album.png" alt="">Mis álbumes</a>
