eb45704ca102cc95a0b34470d4fd1a752c5af244
Alejandro Domenech <a.domenechvicedo@gmail.com>
2017-11-29 18:44:45 +0100

    la i jiji

--- PI/practica8/dar_baja.php
+++ PI/practica8/dar_baja.php
@@ -2,7 +2,7 @@
 	session_start();
 	require_once("plantillas/conexion.php");
 
-	$delete = "delete from usuarios where idUsuario = ".$_GET['id'].";";
+	$delete = "delete from usuarios where IdUsuario = ".$_GET['id'].";";
 
 	if(!($res=$mysqli->query($delete))){
 			echo "Error al ejecutar la sentencia";
